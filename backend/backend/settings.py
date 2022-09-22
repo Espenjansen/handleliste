210dda6570b310ddc58c96fb6e0ba3260b8bbcf3
Espenjansen <ejansen1201@gmail.com>
2022-09-22 20:09:52 +0200

    tailwind fix sort of

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from email.mime import base
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 
 LOGOUT_REDIRECT_URL = "/login"
